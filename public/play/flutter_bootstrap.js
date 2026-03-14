ce4841fe26c5dedd77ad5009e88829ddea1ba537
Kohichi Matsumoto <Kohichi Matsumoto@homemade51.noreply.github.com>
2026-03-14 19:41:07 +0900

    fix phantom web pay launch and shop back flow

--- public/play/flutter_bootstrap.js
+++ public/play/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "722407722" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1951050608" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
